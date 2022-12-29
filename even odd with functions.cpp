956d82a26758ee494f7a1d0e12c45e554ad1967d
shazaibliaqat <121227980+shazaibliaqat@users.noreply.github.com>
2022-12-29 23:27:43 +0500

    Add files via upload

---
+++ even odd with functions.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int case1 (int x)
+{
+  
+   if(x%2==0){
+   cout<<"even no";}
+   else 
+   cout<<"odd no";   
+}
+
+
+int main()
+{
+int y;
+cout<<"enter number =";
+cin>>y;
+case1(x);	
+return 0;
+}
+
